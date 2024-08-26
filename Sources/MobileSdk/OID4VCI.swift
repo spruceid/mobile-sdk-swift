50a5ab6e1ad512bb2795351337f54b896a4eab6a
Tiago Nascimento <tiago.nascimento@spruceid.com>
2024-08-26 13:45:43 -0300

    Remove oid4vci wrapper; Add async client interface impl
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

--- Sources/MobileSdk/OID4VCI.swift
+++ Sources/MobileSdk/OID4VCI.swift
@@ -2,7 +2,7 @@ import Foundation
 
 import SpruceIDMobileSdkRs
 
-public class Oid4vciDefaultHttpClient : SpruceIDMobileSdkRs.HttpClient {
+public class Oid4vciSyncHttpClient : SpruceIDMobileSdkRs.HttpClient {
     public func httpClient(request: HttpRequest) throws -> HttpResponse {
         guard let url = URL(string: request.url) else {
             throw HttpClientError.Other(error: "failed to construct URL")
@@ -68,93 +68,51 @@ public class Oid4vciDefaultHttpClient : SpruceIDMobileSdkRs.HttpClient {
     }
 }
 
-public class Oid4vci {
-    let httpClient: SpruceIDMobileSdkRs.HttpClient;
-    
-    public init(httpClient: SpruceIDMobileSdkRs.HttpClient) {
-        self.httpClient = httpClient
-    }
-    
-    public convenience init() {
-        self.init(httpClient: Oid4vciDefaultHttpClient())
-    }
-
-    public func getMetadata(
-        session: Oid4vciSession
-    ) async throws -> Oid4vciMetadata {
-        try await SpruceIDMobileSdkRs.oid4vciGetMetadata(
-            session: session)
-    }
-    
-    public func initiateWithOffer(
-        credentialOffer: String,
-        clientId: String,
-        redirectUrl: String
-    ) async throws -> Oid4vciSession {
-        try await SpruceIDMobileSdkRs.oid4vciInitiateWithOffer(
-            credentialOffer: credentialOffer,
-            clientId: clientId,
-            redirectUrl: redirectUrl,
-            httpClient: self.httpClient)
-    }
-    
-    public func initiate(
-        baseUrl: String,
-        clientId: String,
-        redirectUrl: String
-    ) async throws -> Oid4vciSession {
-        try await SpruceIDMobileSdkRs.oid4vciInitiate(
-            baseUrl: baseUrl,
-            clientId: clientId,
-            redirectUrl: redirectUrl,
-            httpClient: self.httpClient)
-    }
-
-    public func exchangeToken(
-        session: Oid4vciSession
-    ) throws -> String? {
-        try SpruceIDMobileSdkRs.oid4vciExchangeToken(
-            session: session,
-            httpClient: self.httpClient)
-    }
-
-    public func exchangeCredential(
-        session: Oid4vciSession,
-        proofsOfPossession: [String]
-    ) async throws -> [SpruceIDMobileSdkRs.CredentialResponse] {
-        try await SpruceIDMobileSdkRs.oid4vciExchangeCredential(
-            session: session,
-            proofsOfPossession: proofsOfPossession,
-            httpClient: self.httpClient)
-    }
-
-    public func generatePopPrepare(
-        audience: String,
-        issuer: String,
-        nonce: String?,
-        vm: String,
-        publicJwk: String,
-        durationInSecs: Int64?
-    ) throws -> [UInt8] {
-        let prepare = try SpruceIDMobileSdkRs.generatePopPrepare(
-            audience: audience,
-            issuer: issuer,
-            nonce: nonce,
-            vm: vm,
-            publicJwk: publicJwk,
-            durationInSecs: durationInSecs)
+public class Oid4vciAsyncHttpClient : SpruceIDMobileSdkRs.AsyncHttpClient {
+    public func httpClient(request: HttpRequest) async throws -> HttpResponse {
+        guard let url = URL(string: request.url) else {
+            throw HttpClientError.Other(error: "failed to construct URL")
+        }
         
-        return [UInt8](prepare)
-    }
-
-    public func generatePopComplete(
-        signingInput: [UInt8],
-        signature: [UInt8]
-    ) throws -> String {
-        let complete = try SpruceIDMobileSdkRs.generatePopComplete(
-            signingInput: Data(signingInput),
-            signature: Data(signature))
+        let session = URLSession.shared
+        var req = URLRequest(url: url,
+                             cachePolicy: .useProtocolCachePolicy,
+                             timeoutInterval: 60)
+        req.httpMethod = request.method
+        req.httpBody = request.body
+        req.allHTTPHeaderFields = request.headers
         
-        return complete
+        let data: Data
+        let response: URLResponse
+        do {
+            (data, response) = try await session.data(for: req)
+        } catch {
+            throw HttpClientError.Other(error: "failed to execute request: \(error)")
+        }
+        
+        guard let response = response as? HTTPURLResponse else {
+            throw HttpClientError.Other(error: "failed to parse response")
+        }
+        
+        guard let statusCode = UInt16(exactly: response.statusCode) else {
+            throw HttpClientError.Other(error: "failed to parse response status code")
+        }
+        
+        let headers = try response.allHeaderFields.map({ (k, v) in
+            guard let k = k as? String else {
+                throw HttpClientError.HeaderParse
+            }
+            
+            guard let v = v as? String else {
+                throw HttpClientError.HeaderParse
+            }
+            
+            return (k, v)
+        })
+        
+        return HttpResponse(
+            statusCode: statusCode,
+            headers: Dictionary(uniqueKeysWithValues: headers),
+            body: data)
     }
 }

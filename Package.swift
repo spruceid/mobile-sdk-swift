spruceid/wallet-sdk-swift

Add MDoc Reader support

Package.swift
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["SpruceIDWalletSdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/spruceid/wallet-sdk-rs.git", from: "0.0.25"),
-        // .package(path: "../wallet-sdk-rs"),
+        // .package(url: "https://github.com/spruceid/wallet-sdk-rs.git", from: "0.0.25"),
+        .package(path: "../wallet-sdk-rs"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
     targets: [

spruceid/mobile-sdk-swift

Package.swift
@@ -14,9 +14,8 @@ let package = Package(
             targets: ["SpruceIDMobileSdk"])
     ],
     dependencies: [
-        // .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", .branch("main")),
-        .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", from: "0.0.27"),
-        // .package(path: "../mobile-sdk-rs"),
+    //  .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", .branch("main")),
+        .package(path: "../mobile-sdk-rs"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
     targets: [